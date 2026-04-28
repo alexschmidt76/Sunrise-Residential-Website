af5c8ab0c12d630a5aac21fc3ebbbcd1b3b0bfc5
alex <alex.schmidt767@gmail.com>
2026-04-27 21:21:14 -0500

    added bootsrap

---
+++ frontend/src/assets/components/Home.jsx
@@ -0,0 +1,10 @@
+const Home = () => {
+    return (
+        <div className="Home">
+            <h1>Header</h1>
+            <p>Some text and junk and what not</p>
+        </div>
+    )
+}
+
+export default Home;

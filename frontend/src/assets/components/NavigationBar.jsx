38cd3996e760757595d7f85972019e45abf9eeed
alex <alex.schmidt767@gmail.com>
2026-04-29 16:02:52 -0500

    fixed some errors

--- frontend/src/assets/components/NavigationBar.jsx
+++ frontend/src/assets/components/NavigationBar.jsx
@@ -2,25 +2,39 @@ import { Container, Navbar, Nav, Button, NavDropdown } from "react-bootstrap";
 
 const NavigationBar = () => {
     return (
-        <Navbar sticky="top" expand="">
-            <Container>
-                <Navbar.Brand href="/">
+        <Navbar sticky="top" expand="false">
+            <Container fluid className="position-relative d-flex align-items-center px-3" style={{ minHeight: '112px' }}>
+                <Navbar.Brand href="/" className="position-absolute">
                     <img
-                        src="./Sunrise Residential Logo"
+                        src="src/assets/images/Sunrise Residential Logo.jpeg"
                         className="d-inline-block align-top"
                         alt="Sunrise Residential LLC Logo"
+                        style={{ width: '320px', height: 'auto' }}
                     />
                 </Navbar.Brand>
-                <Button type="submit" href="/contact-us" className="position-absolute start-50 translate-middle-x">Contact Us</Button>
-                <Navbar.Toggle aria-controls="main-navbar" />
-                <Navbar.Collapse id="main-navbar" className="">
-                    <Nav className="justify-content-right">
+
+                <Button 
+                    size="lg"
+                    type="submit" 
+                    href="/contact-us" 
+                    className="position-absolute start-50 translate-middle-x"
+                >
+                    Contact Us
+                </Button>
+                
+                <div className="position-absolute end-0 px-3">
+                    <Navbar.Toggle size="lg" aria-controls="main-navbar" />
+                </div>
+                
+                <Navbar.Collapse id="main-navbar" className="position-absolute end-0 px-3" style={{ top: '100%' }}>
+                    <Nav className="flex-column bg-white border rounded shadow-sm">
                         <Nav.Link href="/contact-us">Contact Us</Nav.Link>
                         <Nav.Link href="/about">About Sunrise Residential</Nav.Link>
                         <Nav.Link href="/photos">Photo Library</Nav.Link>
                         <Nav.Link href="/pricing">Pricing Information</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
+                
             </Container>
         </Navbar>
     )

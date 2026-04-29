2f5b87fcc75845f8e48c0d9d63738fdd4edcf713
alex <alex.schmidt767@gmail.com>
2026-04-29 17:32:11 -0500

    animated hamburger button

--- frontend/src/assets/components/NavigationBar.jsx
+++ frontend/src/assets/components/NavigationBar.jsx
@@ -1,6 +1,10 @@
-import { Container, Navbar, Nav, Button, NavDropdown } from "react-bootstrap";
+import { useState } from "react";
+import { Container, Navbar, Nav, Button } from "react-bootstrap";
+import "../style_sheets/HamburgerMenu.css";
 
 const NavigationBar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
     return (
         <Navbar sticky="top" expand="false">
             <Container fluid className="position-relative d-flex align-items-center px-3" style={{ minHeight: '112px' }}>
@@ -23,17 +27,28 @@ const NavigationBar = () => {
                 </Button>
                 
                 <div className="position-absolute end-0 px-3">
-                    <Navbar.Toggle size="lg" aria-controls="main-navbar" />
+                    <button 
+                        className={`hamburger ${isOpen ? 'open' : ''}`}
+                        onClick={() => setIsOpen(!isOpen)}
+                        type="button"
+                        aria-label="Toggle navigation"
+                    >
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </button>
                 </div>
                 
-                <Navbar.Collapse id="main-navbar" className="position-absolute end-0 px-3" style={{ top: '100%' }}>
-                    <Nav className="flex-column bg-white border rounded shadow-sm">
-                        <Nav.Link href="/contact-us">Contact Us</Nav.Link>
-                        <Nav.Link href="/about">About Sunrise Residential</Nav.Link>
-                        <Nav.Link href="/photos">Photo Library</Nav.Link>
-                        <Nav.Link href="/pricing">Pricing Information</Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
+                {isOpen && (
+                    <div className="position-absolute end-0 px-3" style={{ top: '100%' }}>
+                        <Nav className="flex-column bg-white border rounded shadow-sm">
+                            <Nav.Link href="/contact-us">Contact Us</Nav.Link>
+                            <Nav.Link href="/about">About Sunrise Residential</Nav.Link>
+                            <Nav.Link href="/photos">Photo Library</Nav.Link>
+                            <Nav.Link href="/pricing">Pricing Information</Nav.Link>
+                        </Nav>
+                    </div>
+                )}
                 
             </Container>
         </Navbar>
